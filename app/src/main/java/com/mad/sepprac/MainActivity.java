5f81234d978a53f2a24a9dfdf02359e77d973e65
MittyBoySour <mitchell_creecy@live.com>
2018-09-09 19:33:00 +1000

    init upload

---
+++ app/src/main/java/com/mad/sepprac/MainActivity.java
@@ -0,0 +1,23 @@
+package com.mad.sepprac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference DBRef = database.getReference("message");
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DBRef.setValue("Hello, World!");
+    }
+}
